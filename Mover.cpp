31ab00ce3c3b23bc3b81ebe35a7d8c69d60106fd
Davlet <sau.kin.san@gmail.com>
2018-04-19 17:42:50 +0300

    Update Mover.cpp

--- Mover.cpp
+++ Mover.cpp
@@ -1,6 +1,10 @@
 #include <Urho3D/Graphics/AnimatedModel.h>
 #include <Urho3D/Graphics/AnimationState.h>
 #include <Urho3D/Scene/Scene.h>
+#include <Urho3D/Physics/RigidBody.h>
+#include <Urho3D/Physics/PhysicsEvents.h>
+#include <Urho3D/Physics/PhysicsWorld.h>
+#include <random>
 
 #include "Mover.h"
 
@@ -8,27 +12,27 @@
 
 Mover::Mover(Context* context) :
     LogicComponent(context),
-    moveSpeed_(0.0f),
-    rotationSpeed_(0.0f)
+    moveSpeed_(0.0f)
 {
     // Only the scene update event is needed: unsubscribe from the rest for optimization
     SetUpdateEventMask(USE_UPDATE);
 }
 
-void Mover::SetParameters(float moveSpeed, float rotationSpeed, const BoundingBox& bounds)
+void Mover::Start()
+{
+	// Компонент вставлен в его ноду. Подписываемся на события
+	SubscribeToEvent(GetNode(), E_NODECOLLISIONSTART, URHO3D_HANDLER(Mover, HandleColissionStart));
+}
+
+void Mover::SetParameters(float moveSpeed, Vector3 dir)
 {
     moveSpeed_ = moveSpeed;
-    rotationSpeed_ = rotationSpeed;
-    bounds_ = bounds;
+	direction_ = dir;
 }
 
 void Mover::Update(float timeStep)
 {
-    node_->Translate(Vector3::FORWARD * moveSpeed_ * timeStep);
-
-    Vector3 pos = node_->GetPosition();
-    if (pos.x_ < bounds_.min_.x_ || pos.x_ > bounds_.max_.x_ || pos.z_ > bounds_.min_.z_ || pos.z_ < bounds_.max_.z_)
-        node_->Yaw(180.0f);
+    node_->Translate(direction_ * moveSpeed_ * timeStep);
 
     AnimatedModel* model = node_->GetComponent<AnimatedModel>(true);
     if (model->GetNumAnimationStates())
@@ -36,4 +40,28 @@ void Mover::Update(float timeStep)
         AnimationState* state = model->GetAnimationStates()[0];
 		state->AddTime(timeStep);
     }
+}
+
+void Mover::HandleColissionStart(StringHash eventType, VariantMap& eventData)
+{
+	using namespace NodeCollisionStart;
+	Node* otherNode = static_cast<Node*>(eventData[P_OTHERNODE].GetPtr());
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::bernoulli_distribution d(0.5); //вероятность 50/50
+
+	if (otherNode->GetName() == "Box" || otherNode->GetName() == "Wall" ||
+		otherNode->GetName() == "Cannonbal")
+	{
+		//не знаем куда повернёт нода
+		if (d(gen))
+		{
+			node_->Yaw(90.0f);
+		}
+		else
+		{
+			node_->Yaw(-90.0f);
+		}
+	}
 }
