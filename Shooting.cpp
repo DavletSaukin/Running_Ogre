5635ba5a13e15094a5e09779869b0fc952d04b18
Davlet <sau.kin.san@gmail.com>
2018-04-19 17:45:17 +0300

    Update Shooting.cpp

--- Shooting.cpp
+++ Shooting.cpp
@@ -1,5 +1,7 @@
 #include <Urho3D\Scene\Scene.h>
 #include <Urho3D/Physics/RigidBody.h>
+#include <Urho3D/Physics/PhysicsEvents.h>
+#include <Urho3D/Physics/PhysicsWorld.h>
 
 #include "Shooting.h"
 
@@ -9,21 +11,35 @@ Shooting::Shooting(Context* context): LogicComponent(context)
 	SetUpdateEventMask(USE_UPDATE);
 }
 
+void Shooting::Start()
+{
+	// Компонент вставлен в его ноду. Подписываемся на события
+	SubscribeToEvent(GetNode(), E_NODECOLLISIONSTART, URHO3D_HANDLER(Shooting, HandleColissionStart));
+}
+
 
 void Shooting::Update(float timeStep)
 {
 	Vector3 currPos = node_->GetPosition();
-	currPos += Vector3::BACK;
+	currPos += direction;
 
-	if (currPos.z_ < box_.max_.z_)
-	{
-		currPos = startPos;
-	}
 	node_->SetPosition(currPos);
 }
 
-void Shooting::SetParameters(Vector3 sPos, BoundingBox box)
+void Shooting::SetParameters(Vector3 sPos, Vector3 dir)
 {
 	startPos = sPos;
-	box_ = box;
+	direction = dir;
+}
+
+void Shooting::HandleColissionStart(StringHash eventType, VariantMap& eventData)
+{
+	using namespace NodeCollisionStart;
+	Node* otherNode = static_cast<Node*>(eventData[P_OTHERNODE].GetPtr());
+
+	if (otherNode->GetName() == "Box" || otherNode->GetName() == "Wall" ||
+		otherNode->GetName() == "Ninja" || otherNode->GetName() == "Cannon")
+	{
+		node_->SetPosition(startPos);
+	}
 }
